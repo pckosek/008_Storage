bc125084d0d01cbae6c3425e9b262ae8f79d2519
pckosek <pckosek@local.tjhsst.edu>
2017-07-18 09:28:31 -0400

    v 1.0

---
+++ app/src/main/java/com/example/pckosek/a008_storage/MainActivity.java
@@ -0,0 +1,34 @@
+package com.example.pckosek.a008_storage;
+
+import android.content.Context;
+import android.content.SharedPreferences;
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import android.util.Log;
+
+public class MainActivity extends AppCompatActivity {
+
+    /* ------------------------*/
+    /*    FILE VERSION 1.0     */
+    /* ------------------------*/
+
+    private final static String TAG = "MainActivity";
+
+    private final static String PREFERENCES_FILE = "my_numbers";
+    private final static String COUNT_KEY = "count";
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+        Context context = MainActivity.this;
+        SharedPreferences sharedPref = context.getSharedPreferences(PREFERENCES_FILE,Context.MODE_PRIVATE);
+
+        int defaultInt = 0;
+        int numCounts = sharedPref.getInt(COUNT_KEY, 0);
+        Log.i(TAG, "NumCounts: "+numCounts);
+
+
+    }
+}
